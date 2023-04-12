b7526907e795d2e66e56b308f98dde7a0db4d25e
Marchmello55 <ulansachilaev2006@gmail.com>
2023-04-12 18:25:06 +0300

    asd

---
+++ geom_lib/circle.py
@@ -0,0 +1,7 @@
+print("введи радиус и что надо найти ")
+a=int(input())
+c=str(input())
+if c == "площадь":
+    print(a*a*3,14)
+else :
+    print(2*a*3,14)  
---
+++ geom_lib/square.py
@@ -0,0 +1,7 @@
+print("введи сторону и что надо найти ")
+a=int(input())
+c=str(input())
+if c == "площадь":
+    print(a**2)
+else :
+    print(a*4)  
